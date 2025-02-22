e845ffd4dcf8fae070efce96a5b74481c24e8109
yamil34 <oscar.robledo.2023@cbtis-246.edu.mx>
2025-02-21 18:35:26 -0600

    Update hockey.sql

--- hockey.sql
+++ hockey.sql
@@ -37,3 +37,59 @@ username VARCHAR(20) NOT NULL,
 password VARCHAR(25) NOT NULL,
 PRIMARY KEY (id)
 );
+
+CREATE TABLE UserInfo (
+id INT (16) NOT NULL,
+firstName VARCHAR(10) NOT NULL,
+lastName VARCHAR(10) NOT NULL,
+email VARCHAR(50) NOT NULL,
+Round1Points VARCHAR(20) NOT NULL,
+Round2Points VARCHAR(20) NOT NULL,
+Round3Points VARCHAR(20) NOT NULL,
+Round4Points VARCHAR(20) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE HockeyTeamPlayer (
+id INt (16) NOT NULL,
+hockeyTeamid VARCHAR(25) NOT NULL,
+firstName VARCHAR (10) NOT NULL,
+lastName VARCHAR (10) NOT NULL,
+jerseyNum VARCHAR (6) NOT NULL,
+position VARCHAR (6) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE UserScorePicks (
+id INT (16) NOT NULL,
+hockeyGameid VARCHAR(20) NOT NULL,
+team1score VARCHAR (15) NOT NULL,
+userid VARCHAR (16) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE UserStartsPicks (
+id INT (16) NOT NULL,
+roudId VARCHAR (3) NOT NULL,
+goalLeaderId VARCHAR(80) NOT NULL,
+assistLeaderId VARCHAR(15) NOT NULL,
+penaltyLeadearId VARCHAR(3) NOT NULL,
+plusMinusLearId VARCHAR(5) NOT NULL,
+faceoffsWonLeaderId VARCHAR(4) NOT NULL,
+sogLeaderId VARCHAR (6) NOT NULL,
+numShutouts VARCHAR (15) NOT NULL,
+userId VARCHAR(16) NOT NULL,
+PRIMARY KEY (id,userId)
+);
+
+CREATE TABLE roundStats (
+id INT (16) NOT NULL,
+numShutouts VARCHAR (15) NOT NULL,
+goalLeaderid VARCHAR (80) NOT NULL,
+assistLeaderid VARCHAR (15) NOT NULL,
+penaltyLeaderid VARCHAR (3) NOT NULL,
+plusMinusLeaderid VARCHAR (5) NOT NULL,
+faceoffsWonLeaderid VARCHAR (4) NOT NULL,
+sogLeaderid VARCHAR (6) NOT NULL,
+PRIMARY KEY (id)
+);
