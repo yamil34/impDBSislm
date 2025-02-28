a5c002c08f9a0dcc5d2712df6023750c18b9637b
yamil34 <oscar.robledo.2023@cbtis-246.edu.mx>
2025-02-27 20:21:02 -0600

    Update usuarios.sql

--- usuarios.sql
+++ usuarios.sql
@@ -5,3 +5,22 @@
   `codigo_incapacidad` varchar(25) DEFAULT NULL,
   PRIMARY KEY (`horario_id`,`hora_salida`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 
+
+CREATE TABLE empleados (
+nameEmpleado VARCHAR (10) NOT NULL,
+departamentoid VARCHAR (15) NOT NULL,
+PRIMARY KEY (departamentoid)
+);
+
+
+
+
+INSERT INTO empleados (nameEmpleado, departamentoid)
+VALUES
+(‘Ernesto’,’3a’),
+(‘alexa’,’2c’),
+(‘armando’,’4c’),
+(‘angel’,’2s’),
+(‘ana’,’5s’,),
+(‘liam’,’2n’),
+
