43845be521be7ce782669bdf80d2d9910a342c21
yamil34 <oscar.robledo.2023@cbtis-246.edu.mx>
2025-02-28 19:00:36 -0600

    Update usuarios.sql

--- usuarios.sql
+++ usuarios.sql
@@ -24,3 +24,24 @@ VALUES
 (‘ana’,’5s’,),
 (‘liam’,’2n’),
 
+CREATE TABLE empleados (
+nombre VARCHAR (10) NOT NULL,
+apellido VARCHAR (15) NOT NULL,
+usuarios_id VARCHAR (10) NOT NULL,
+departamento VARCHAR (10) NOT NULL,
+PRIMARY KEY (nombre, apellido, usuarios_id, departamento )
+);
+
+
+insert into empleados (usuarios_id, nombre, apellido, departamento)
+    -> values (2, 'ximena', 'marin', 'XL'),
+    ->        (3, 'abigail', 'huerta', 'JC'),
+    ->        (4, 'alexis', 'cabañas', 'CJ'),
+    ->        (5, 'camila', 'rodriguez', 'RD'),
+    ->        (6, 'jesus', 'enrique', 'XP'),
+    ->        (7, 'alexa', 'paola', 'QM'),
+    ->        (8, 'david', 'lozano', 'PEN'),
+    ->        (9, 'toledo', 'guadalupe', 'OD'),
+    ->        (10, 'dulce', 'silva', 'DK');
+
+
